835ecd91664be7f3bfc9d4518bd0cf3e25fa89f4
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-06-05 21:34:11 +0700

    Bottom Navigation Bar HomeScreen

--- SnappyShop/app/src/main/java/com/example/snappyshop/screen/HomeScreen.kt
+++ SnappyShop/app/src/main/java/com/example/snappyshop/screen/HomeScreen.kt
@@ -4,35 +4,81 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.snappyshop.pages.CartPage
+import com.example.snappyshop.pages.FavoritePage
+import com.example.snappyshop.pages.HomePage
+import com.example.snappyshop.pages.ProfilePage
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier, navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(32.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Home Screen")
-        Button(
-            onClick = {
-                Firebase.auth.signOut()
-                navController.navigate("auth") {
-                    popUpTo("home") { inclusive = true }
+
+    val navItemList = listOf<NavItem>(
+        NavItem("Home", Icons.Default.Home),
+        NavItem("Favorite", Icons.Default.Favorite),
+        NavItem("Cart", Icons.Default.ShoppingCart),
+        NavItem("Profile", Icons.Default.Person)
+    )
+
+    var selectedIndex by remember {
+        mutableStateOf(0)
+    }
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar { 
+                navItemList.forEachIndexed { index, navItem ->
+                    NavigationBarItem(
+                        selected = index == selectedIndex,
+                        onClick = {
+                            selectedIndex = index
+                        },
+                        icon = {
+                            Icon(imageVector = navItem.icon, contentDescription = navItem.label)
+                        },
+                        label = {Text(text = navItem.label)}
+                    )
                 }
-            },
-        ) {
-            Text(text = "Log out")
+            }
         }
+    ) {
+        ContentScreen(modifier = Modifier.padding(it), selectedIndex)
     }
 }
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier, selectedIndex: Int) {
+    when(selectedIndex) {
+        0 -> HomePage(modifier)
+        1 -> FavoritePage(modifier)
+        2 -> CartPage(modifier)
+        3 -> ProfilePage(modifier)
+    }
+}
+
+data class NavItem (
+    val label: String,
+    val icon: ImageVector
+)
