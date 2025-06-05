cff6ba005011bb51d749b68797af7d3185c2e487
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-06-05 22:56:27 +0700

    BannerView for HomeScreen

--- SnappyShop/app/src/main/java/com/example/snappyshop/pages/HomePage.kt
+++ SnappyShop/app/src/main/java/com/example/snappyshop/pages/HomePage.kt
@@ -1,10 +1,25 @@
 package com.example.snappyshop.pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.snappyshop.components.BannerView
+import com.example.snappyshop.components.HeaderView
 
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
-    Text(text = "Home")
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(16.dp))
+    {
+        HeaderView(modifier)
+        Spacer(modifier = Modifier.height(10.dp))
+        BannerView(modifier = Modifier.height(200.dp))
+    }
 }
