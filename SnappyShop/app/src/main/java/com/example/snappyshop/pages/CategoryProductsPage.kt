0e5e6f742b5389364c1b63dcf4fcf549d363ad3e
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-06-06 19:11:23 +0700

    Products Design

--- SnappyShop/app/src/main/java/com/example/snappyshop/pages/CategoryProductsPage.kt
+++ SnappyShop/app/src/main/java/com/example/snappyshop/pages/CategoryProductsPage.kt
@@ -1,19 +1,18 @@
 package com.example.snappyshop.pages
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.snappyshop.model.CategoryModel
+import com.example.snappyshop.components.ProductItemView
 import com.example.snappyshop.model.ProductModel
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
@@ -34,14 +33,23 @@ fun CategoryProductPage(modifier: Modifier = Modifier, categoryId: String) {
                     productList.value = it.result.documents.mapNotNull { doc ->
                         doc.toObject(ProductModel::class.java)
                     }
+
+                    // productList.value = resultList.plus(resultList).plus(resultList)
+                    // Add more items for testing purpose
                 }
             }
     }
 
-    LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        items(productList.value) {item ->
-            Text(text = item.name)
-            Spacer(modifier = Modifier.height(50.dp))
+    LazyColumn(modifier = Modifier.fillMaxSize().padding(horizontal = 10.dp, vertical = 30.dp)) {
+        items(productList.value.chunked(2)) {rowItems ->
+            Row {
+                rowItems.forEach {
+                    ProductItemView(product = it, modifier = Modifier.weight(1f))
+                }
+                if (rowItems.size == 1) {
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
         }
     }
 }
