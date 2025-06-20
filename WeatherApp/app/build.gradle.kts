d2c78152d009a0b800dbc5454d9662d5c60ff20e
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-06-20 10:33:55 +0700

    View Model + Bug: The compiler option dagger.hilt.android.internal.projectType is not a recognized Hilt option. Is there a typo?. ComponentProcessingStep was unable to process 'com.example.weatherapp.WeatherApp_HiltComponents.SingletonC' because 'dagger.hilt.android.internal.lifecycle.DefaultActivityViewModelFactory' could not be resolved. -> Check Dependency

--- WeatherApp/app/build.gradle.kts
+++ WeatherApp/app/build.gradle.kts
@@ -60,11 +60,10 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     // Dagger - Hilt
-    implementation ("com.google.dagger:hilt-android:2.40.5")
-    kapt ("com.google.dagger:hilt-android-compiler:2.40.5")
-    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
-    kapt ("androidx.hilt:hilt-compiler:1.0.0")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation ("com.google.dagger:hilt-android:2.56.2")
+    kapt ("com.google.dagger:hilt-android-compiler:2.56.2")
+    kapt ("androidx.hilt:hilt-compiler:1.2.0")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Location Services
     implementation ("com.google.android.gms:play-services-location:20.0.0")
