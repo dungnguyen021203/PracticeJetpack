d665c256b34505142128bbc3553d9fa829a74765
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-06-20 09:43:39 +0700

    Dto, Mapper, Data, Api

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/data/remote/WeatherApi.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+    @GET("v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl")
+    suspend fun getWeatherData(
+        @Query("longitude") long: Double,
+        @Query("latitude") lat: Double
+    ): WeatherDto
+}
